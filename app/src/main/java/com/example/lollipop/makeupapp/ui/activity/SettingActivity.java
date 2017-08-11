029f557b2210faf25b2a397f7ca8b3396cb69bc7
R2D2 <zhuyuexx@foxmail.com>
2017-08-11 11:45:00 +0800

    1.ViewPager出现的问题是因为在Fragment中嵌套了ViewPager，获取FragmentManager时使用了getSupportFragmentManager()方法，与父级共用了一个FragmentManager，所以有问题，解决方法是使用getChildFragmentManager()方法。 2.大致完成设置页面和个人信息页面 3.逐步开始写功能逻辑

--- app/src/main/java/com/example/lollipop/makeupapp/ui/activity/SettingActivity.java
+++ app/src/main/java/com/example/lollipop/makeupapp/ui/activity/SettingActivity.java
@@ -1,15 +1,25 @@
 package com.example.lollipop.makeupapp.ui.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.lollipop.makeupapp.R;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class SettingActivity extends AppCompatActivity {
+
+    @OnClick(R.id.personal_info)
+    void personalInfo(){
+        startActivity(new Intent(this, PersonalInfoActivity.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        ButterKnife.bind(this);
     }
 }
